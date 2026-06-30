c5ffd4fdb3cd7055f107499c946633c272b0a27f
lecareli <leticia.careli@outlook.com>
2026-06-30 13:06:42 -0300

    Add Pessoa model and migrations for pessoas and users tables
    - Created the Pessoa model to represent the 'pessoas' table in the database.
    - Added migration for the 'pessoas' table with relevant fields including 'eh_ativo', 'nome_completo', and address details.
    - Introduced migration for the 'users' table, establishing a foreign key relationship with the 'pessoas' table and including fields for user management.
    - Updated sidebar layout to replace 'Usuários' with 'Pessoas' for improved clarity in navigation.

--- database/migrations/2026_06_30_082500_create_users_table.php
+++ database/migrations/2026_06_30_082500_create_users_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Pessoa;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -13,6 +14,8 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
+            $table->foreignIdFor(Pessoa::class, 'pessoa_id')->nullable()->constrained('pessoas')->nullOnDelete();
+            $table->boolean('is_active')->default(true);
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
--- resources/views/components/layouts/sidebar.blade.php
+++ resources/views/components/layouts/sidebar.blade.php
@@ -24,12 +24,8 @@
                         'bg-gray-100 text-gray-800 dark:bg-neutral-700 dark:text-neutral-200' => request()->routeIs('dashboard'),
                         'text-gray-800 hover:bg-gray-100 dark:text-neutral-200 dark:hover:bg-neutral-700' => !request()->routeIs('dashboard'),
                     ])>
-                        <svg class="shrink-0 size-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24"
-                            viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
-                            stroke-linecap="round" stroke-linejoin="round">
-                            <path d="m3 9 9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z" />
-                            <polyline points="9 22 9 12 15 12 15 22" />
-                        </svg>
+                        <i class="bi bi-house {{ request()->routeIs('dashboard') ? 'hidden' : '' }}"></i>
+                        <i class="bi bi-house-fill {{ request()->routeIs('dashboard') ? '' : 'hidden' }}"></i>
                         Página inicial
                     </a>
                 </li>
@@ -38,14 +34,10 @@
                 <li x-data="{ open: false }">
                     <button type="button" @click="open = !open"
                         class="w-full flex items-center gap-x-3 py-2 px-2.5 text-sm text-gray-800 dark:text-neutral-200 rounded-lg hover:bg-gray-100 dark:hover:bg-neutral-700">
-                        <svg class="shrink-0 size-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24"
-                            viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
-                            stroke-linecap="round" stroke-linejoin="round">
-                            <path d="M16 21v-2a4 4 0 0 0-4-4H6a4 4 0 0 0-4 4v2" />
-                            <circle cx="9" cy="7" r="4" />
-                            <path d="M22 21v-2a4 4 0 0 0-3-3.87" />
-                            <path d="M16 3.13a4 4 0 0 1 0 7.75" />
-                        </svg>
+
+                        <i class="bi bi-collection" :class="open && 'hidden'"></i>
+                        <i class="bi bi-collection-fill" :class="!open && 'hidden'"></i>
+
                         Cadastros
                         <svg class="ms-auto size-4 transition-transform" :class="open && 'rotate-180'"
                             xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
@@ -58,7 +50,7 @@
                         <li>
                             <a href="#"
                                 class="flex py-2 px-2.5 text-sm text-gray-800 dark:text-neutral-200 rounded-lg hover:bg-gray-100 dark:hover:bg-neutral-700">
-                                Usuários
+                                Pessoas
                             </a>
                         </li>
                         <!-- <li>
