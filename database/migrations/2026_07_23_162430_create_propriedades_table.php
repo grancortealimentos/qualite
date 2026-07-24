9adb943fafc2d992733355f396804d03e51941a9
lecareli <leticia.careli@outlook.com>
2026-07-24 10:36:29 -0300

    Add additional fields to propriedades migration for enhanced data structure</message>
    <message>Updated the migration for the 'propriedades' table to include new fields such as 'produtor_id', 'car', various area measurements, address details, and geographical coordinates. This enhancement improves the data structure for managing propriedade entities, allowing for more comprehensive information storage and retrieval.

--- database/migrations/2026_07_23_162430_create_propriedades_table.php
+++ database/migrations/2026_07_23_162430_create_propriedades_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Pessoa;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -10,13 +11,31 @@ return new class extends Migration
     {
         Schema::create('propriedades', function (Blueprint $table) {
             $table->id();
+            $table->foreignIdFor(Pessoa::class, 'produtor_id');
             $table->boolean('eh_ativo')->default(true);
             $table->string('razao_social');
             $table->string('nome_fantasia')->nullable();
             $table->string('cnpj', 14)->unique();
             $table->string('ie', 20)->nullable();
             $table->string('nrif', 50)->nullable();
+            $table->string('car', 10)->nullable();
+            $table->decimal('area_total', 10, 2)->nullable();
+            $table->decimal('area_consolidada', 10, 2)->nullable();
+            $table->decimal('area_reservada_legal', 10, 2)->nullable();
+            $table->decimal('area_app', 10, 2)->nullable();
+            $table->integer('capacidade_armazenamento')->nullable();
+            $table->string('cep', 8)->nullable();
+            $table->string('logradouro')->nullable();
+            $table->string('numero')->nullable();
+            $table->string('bairro')->nullable();
+            $table->string('cidade')->nullable();
+            $table->string('estado')->nullable();
+            $table->string('pais')->nullable()->default('Brasil');
+            $table->string('complemento')->nullable();
+            $table->string('latitude')->nullable();
+            $table->string('longitude')->nullable();
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
